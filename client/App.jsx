5445e4ba53d585d91f3cd0cc9d38027dacdf7632
Luke Brennan <LHB@Lukes-MacBook-Air-2.local>
2018-07-08 15:40:25 -0700

    implementing audio scheduling

--- client/App.jsx
+++ client/App.jsx
@@ -8,44 +8,23 @@ import MasterControl from './components/MasterControl.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      audioContext: new AudioContext(),
+      audioContext: null,
       timerId: null,
-      timerInterval: 100, // milliseconds
+      timerInterval: 50, // milliseconds
       playing: false,
       bars: 1,
       resolution: 16, // steps per bar
       scheduleAheadTime: .1, // seconds
       nextStepTime: 0, // seconds
       activeStep: 0, // 16 total steps
-      bpm: 125, // beats per minute
+      bpm: 60, // beats per minute
       swing: 3,
-      instruments: [
-        'kick',
-        'clap',
-        'snare',
-        'openHat',
-        'closedHat',
-      ],
-      pattern: [
-        {kick: false, clap: false, snare: false, openHat: false, closedHat: false},
-        {kick: false, clap: false, snare: false, openHat: false, closedHat: false},
-        {kick: false, clap: false, snare: false, openHat: false, closedHat: false},
-        {kick: false, clap: false, snare: false, openHat: false, closedHat: false},
-        {kick: false, clap: false, snare: false, openHat: false, closedHat: false},
-        {kick: false, clap: false, snare: false, openHat: false, closedHat: false},
-        {kick: false, clap: false, snare: false, openHat: false, closedHat: false},
-        {kick: false, clap: false, snare: false, openHat: false, closedHat: false},
-        {kick: false, clap: false, snare: false, openHat: false, closedHat: false},
-        {kick: false, clap: false, snare: false, openHat: false, closedHat: false},
-        {kick: false, clap: false, snare: false, openHat: false, closedHat: false},
-        {kick: false, clap: false, snare: false, openHat: false, closedHat: false},
-        {kick: false, clap: false, snare: false, openHat: false, closedHat: false},
-        {kick: false, clap: false, snare: false, openHat: false, closedHat: false},
-        {kick: false, clap: false, snare: false, openHat: false, closedHat: false},
-        {kick: false, clap: false, snare: false, openHat: false, closedHat: false},
-      ],
+      instruments: ['kick', 'clap', 'snare', 'openHat', 'closedHat'],
+      pattern: new Array(16).fill().map(elem => ({kick: false, clap: false, snare: false, openHat: false, closedHat: false})),
     };
+
     this.sounds = {
       kick: new pizz.Sound('/samples/SampleMagic_tr909_kick_04.wav'),
       clap: new pizz.Sound('/samples/SampleMagic_tr909_clap.wav'),
@@ -57,54 +36,79 @@ class App extends React.Component {
     this.updatePattern = this.updatePattern.bind(this);
     this.triggerSample = this.triggerSample.bind(this);
     this.play = this.play.bind(this);
+    this.timer = this.timer.bind(this);
+    this.scheduler = this.scheduler.bind(this);
+    this.nextStep = this.nextStep.bind(this);
   }
 
   play() {
     this.state.playing = !this.state.playing;
     console.log(`playing: ${this.state.playing}`)
 
-    if (playing) { // start playing
+    if (this.state.playing) { // start playing
         this.setState({
+          audioContext: new AudioContext(),
           activeStep: 0,
-          nextStepTime: audioContext.currentTime,
+          nextStepTime: 0,
         })
-        timer();
+        this.timer();
     } else {
-        clearInterval(this.state.timeId);
+        clearInterval(this.state.timerId);
     }
   }
 
   timer() {
-    this.state.timerId = setInterval(scheduler, timerInterval);
+    const timerId = setInterval(this.scheduler, this.state.timerInterval);
+    this.setState({timerId: timerId});
+    console.log('timerId: ', this.state.timerId);
   }
 
   scheduler() {
-    const { nextStepTime, audioContext, scheduleAheadTime } = this.state;
+    const { audioContext, scheduleAheadTime } = this.state;
     // while there are notes that will need to play before the next interval, 
     // schedule them and advance the pointer.
-    while (nextStepTime < audioContext.currentTime + scheduleAheadTime ) {
-        this.sounds.kick(nextStepTime);
-        nextStep();
+    console.log('running scheduler...');
+    while (this.state.nextStepTime < audioContext.currentTime + scheduleAheadTime ) {
+        console.log('nextStepTime', this.state.nextStepTime);
+        this.sounds.kick.play(this.state.nextStepTime);
+        this.nextStep();
     }
   }
 
   nextStep() {
-    var secondsPerBeat = 60.0 / bpm;
-    nextStepTime += 0.25 * secondsPerBeat;
+    const secondsPerBeat = 60.0 / this.state.bpm;
+    this.setState(prevState => (
+      {nextStepTime: prevState.nextStepTime + (0.25 * secondsPerBeat)}
+    ));
 
-    activeStep++;
-    if (activeStep === 16) {
-      activeStep = 0;
-    }
+    this.setState(prevState => {
+      let newActiveStep = prevState.activeStep + 1;
+      if (newActiveStep === 16) {
+        return {
+          activeStep: 0
+        };        
+      }
+      return {
+        activeStep: newActiveStep
+      };     
+    });
   }
 
   triggerSample(instrument) {
-    this.sounds[instrument].play();
+    // this.sounds[instrument].play();
+    console.log('now: ', this.state.audioContext.currentTime);
+    this.sounds.kick.play(.25);
+    // this.sounds.kick.play(.5);
+    // this.sounds.kick.play(.75);
+    // this.sounds.kick.play(1);
+    // this.sounds.kick.play(1.25);
+    // this.sounds.kick.play(1.5);
+    // this.sounds.kick.play(1.75);
+    this.sounds.kick.play(2);
   }
 
   updatePattern(instrument, beat, subBeat) {
     const stepNum = ((beat - 1) * 4) + subBeat - 1;
-    console.log('stepNum', stepNum);
     this.setState((prevState) => {
       const newPattern = [...prevState.pattern];
       newPattern[stepNum][instrument] = !prevState.pattern[stepNum][instrument];
