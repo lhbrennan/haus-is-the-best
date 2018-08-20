1a0e0eba3c6f64e80440dd1dbc3a67d1754aa3d5
Luke Brennan <LHB@Lukes-MacBook-Air-2.local>
2018-08-19 19:13:21 -0700

    refactor App to functional stateless component

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { connect } from 'react-redux';
 
 import PerformerSectionContainer from './containers/PerformerSectionContainer';
 import MasterControlContainer from './containers/MasterControlContainer';
@@ -11,36 +10,11 @@ flex-direction: column;
 justify-content: center;
 `;
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      bars: 1,
-      resolution: 16, // steps per bar
-      padResponse: false,
-    };
+const App = () => (
+  <Wrapper>
+    <MasterControlContainer />
+    <PerformerSectionContainer />
+  </Wrapper>
+);
 
-    this.togglePadResponse = this.togglePadResponse.bind(this);
-  }
-
-  /*---------------------------------------------------------------------------
-    EVENT HANDLERS
-  ---------------------------------------------------------------------------*/
-  togglePadResponse() {
-    this.setState(prevState => ({ padResponse: !prevState.padResponse }));
-  }
-  /*---------------------------------------------------------------------------
-    RENDERING
-  ---------------------------------------------------------------------------*/
-
-  render() {
-    return (
-      <Wrapper>
-        <MasterControlContainer />
-        <PerformerSectionContainer />
-      </Wrapper>
-    );
-  }
-}
-
-export default connect()(App);
+export default App;
