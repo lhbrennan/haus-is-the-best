e274dd990936949ed032f16e72005dfcb72ea296
Luke Brennan <lhbrennan@gmail.com>
2018-11-02 13:14:48 -0700

    configure aws-sdk

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const parser = require('body-parser');
+var AWS = require('aws-sdk');
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+var s3 = new AWS.S3();
 
 // const db = require('../database/db.js');
 
@@ -8,8 +16,23 @@ const app = express();
 const port = process.env.PORT || 3000;
 // static assets should be referenced with absolute paths
 app.use('/', express.static('public'));
-app.use('/samples', express.static('samples'));
+// app.use('/samples', express.static('samples'));
 app.use('/compositions', parser.json());
+
+app.get('/samples/:id', (req, res) => {
+  const bucketParams = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: req.params.id,
+  };
+  s3.getObject(bucketParams, (err, data) => {
+    if (err) {
+      console.log(err, err.stack);
+    } else {
+      console.log(data);
+      res.status(200).send(data);
+    }
+  });
+});
 /*
 app.get('/compositions', (req, res) => {
   const { username, compositionName } = req.query;
