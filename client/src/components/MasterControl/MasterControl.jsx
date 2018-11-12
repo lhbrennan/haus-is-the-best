6349b17c480f00bc2e4ccdfa16ab65afde05a936
Luke Brennan <lhbrennan@gmail.com>
2018-11-12 10:03:51 -0800

    cleanup unused styled components

--- client/src/components/MasterControl/MasterControl.jsx
+++ client/src/components/MasterControl/MasterControl.jsx
@@ -3,36 +3,13 @@ import styled from 'styled-components';
 
 import PlayBackSettingsContainer from '../PlayBackSettings/PlayBackSettingsContainer';
 import Bars from '../Bars/Bars';
-import ButtonTemplate from '../UI/Button';
+import Button from '../UI/Button';
 
 const Wrapper = styled.div`
   display: flex;
   flex-flow: row;
   justify-content: flex-start;
   margin: 0px 0px 25px 0px;
-`;
-
-const Button = styled(ButtonTemplate)`
-`;
-
-const Button2 = styled.button`
-  background: ${({ padResponse }) => {
-    if (padResponse) { return 'orange'; }
-    return '#34495e';
-  }};
-  margin: 0px 0px 5px 0px;
-  Width: 80px;
-  height: 60px;
-  font-size: 22px;
-  color: white;
-  text-align: center;
-  text-shadow: 0 1px 2px rgba(0, 0, 0, 0.25);
-  border: 0;
-  border-bottom: 2px solid #263849;
-  border-radius: 5px;
-  cursor: pointer;
-  -webkit-box-shadow: inset 0 -2px #263849;
-  box-shadow: inset 0 -2px #263849;
 `;
 
 const Div = styled.div`
